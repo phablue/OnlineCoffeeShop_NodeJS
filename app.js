42fb0f450242829fa0a4ffd13a122cc23e044a26
hyunahpark <phablue@gmail.com>
2014-11-28 21:59:11 -0600

    add login logout

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var jade = require("jade");
 var path = require("path");
 var bodyParser = require("body-parser");
+var sessions = require("express-session");
 var express = require("express");
 var app = express();
 var server = require("http").Server(app);
@@ -9,19 +10,30 @@ var cartSocket = require("./custom_modules/cart.js");
 var home = require("./routes/home");
 var cart = require("./routes/cart");
 var user = require("./routes/user");
+var session = require("./routes/session");
 
 app.set("view engine", "jade");
 
 app.use(express.Router());
 app.use(bodyParser());
+app.use(sessions({ secret: "secret key" }));
+
 app.use("/public", express.static(path.join(__dirname, "public")));
 app.use("/bower_components", express.static(path.join(__dirname, "bower_components")));
+app.use(function(req, res, next){
+  res.locals.session = req.session;
+  next();
+});
 
 app.get("/", home.index);
 
 app.get("/cart", cart.index);
 app.post("/cart/:name/update", cart.update);
 app.get("/cart/:name/delete", cart.delete);
+
+app.get("/login", session.new);
+app.post("/login", session.create);
+app.get("/logout", session.delete);
 
 app.get("/users/new", user.new);
 app.post("/users/new", user.create);
---
+++ routes/session.js
@@ -0,0 +1,49 @@
+var crypto = require("crypto");
+var sqlClient = require("mysql").createConnection({user: "root", password: "root", database: "CoffeeShop"});
+
+var messages = "";
+
+var authenticate = function (req, res, result) {
+  if (result == null) {
+    messages = "Sorry, This Email Does Not Exist. Please Try Again.";
+    res.redirect("/login");
+  } 
+  else if (wrongPassword(result[0].PassWord, req.body.password)) {
+    messages = "Sorry, This Password Is Not Correct. Please Try Again.";
+    res.redirect("/login");      
+  }
+  else {
+    messages = "";
+    req.session.userEmail = req.body.email;
+    req.session.userName = result[0].FirstName;
+    res.redirect("/");
+  }
+};
+
+var wrongPassword = function (password, input) {
+  return password != encrypt(input);
+};
+
+exports.new = function (req, res) {
+  res.render("sessions/new", {messages: messages});
+};
+
+exports.create = function (req, res) {
+  sqlClient.query("select PassWord, FirstName from users where EMail = ?", [req.body.email],
+  function (err, result) {
+    if (err) throw err;
+    authenticate(req, res, result);
+  });
+};
+
+exports.delete = function (req, res) {
+  req.session.userEmail = null;
+  req.session.userName = null;
+  res.redirect("/");
+};
+
+var encrypt = function (password) {
+  var shasum = crypto.createHash("sha1")
+  shasum.update(password);
+  return shasum.digest("base64");
+};
